16633d7bbb9852c3a5259b1a2ec8d9db9315ae1e
mauro1012 <mauricio.correa369@gmail.com>
2025-12-09 18:50:36 -0500

    chanes backend.tf and main.tf to update the S3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mauro-terraform-state-bucket"   
+    bucket         = "test-fist-quest"   
     key            = "sitio001/terraform.tfstate"     
     region         = "us-east-1"                      
     encrypt        = true                             
