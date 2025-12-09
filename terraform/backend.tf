77a2e4ea00cca08f404c093952bcd025df5e4718
mauro1012 <mauricio.correa369@gmail.com>
2025-12-09 18:56:16 -0500

    changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-fist-quest"   
+    bucket         = "mlknljnjlbh"   
     key            = "sitio001/terraform.tfstate"     
     region         = "us-east-1"                      
     encrypt        = true                             
